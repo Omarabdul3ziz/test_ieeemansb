470c8bc086774f4992128f30054665cf4ab5bab8
omarabdul3ziz <omarabdul3ziz@gmail.com>
2020-04-14 15:52:37 +0200

    first page backend

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
+from app import views
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+
+urlpatterns = [url(r"^admin/", admin.site.urls), path("hello", views.greet)]
+
